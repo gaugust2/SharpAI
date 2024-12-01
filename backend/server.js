879fe8ef6ba55ca5d3a699e06e941114b30e5769
oadelek01 <114633115+oadelek01@users.noreply.github.com>
2024-12-01 18:03:24 -0500

    bettingForn info

--- backend/server.js
+++ backend/server.js
@@ -9,44 +9,26 @@ const { sportsDataIOController } = require('./sportsDataIOService')
 app.get('/', async (req, res) => {
   res.send('Hello World!')
 });
-
   
+
 app.post('/betslip', async (req, res) => {
     try {
-        const betslip = req.body;
-        /* Bet slip should have the following structure:
-        {
-            "league": "NBA",
-            "selectedTeam": {
-                "city": "Dallas",
-                "name": "Mavericks"
-            },
-            "opposingTeam": {
-                "city": "Utah",
-                "name": "Jazz"
-            },
-            "date": "November 30 2024"
-        }
-
-        Below are acceptable date formats from the user. Anything else will result in an error.
-        October 22 2024
-        10/22/2024
-        10-22-2024
-        2024/10/22
-        */
-        
-        const externalData = await sportsDataIOController(betslip);
-        //externalData is an array of the following: [selectedTeamLast5Games, opposingTeamLast5Games, selectedTeamInjuriesList, opposingTeamInjuriesList, projections]
-        //They are all JSON objects that we will use to inform our analysis
-        const analysis = await getAnalysis(externalData);
-        
-        res.send(externalData)
-        //res.json({ analysis });
+      const betslip = req.body;
+  
+      // Validate the input structure
+      if (!betslip.league || !betslip.selectedTeam || !betslip.opposingTeam || !betslip.date) {
+        throw new Error('Invalid betslip structure.');
+      }
+  
+      const externalData = await sportsDataIOController(betslip);
+      const analysis = await getAnalysis(externalData);
+  
+      res.json({ analysis });
     } catch (error) {
-        console.error(error.message);
-        res.status(404).send(error.message);
+      console.error(error.message);
+      res.status(404).send(error.message);
     }
-});
+  });
 
 const PORT = 3000
 app.listen(PORT, () => {
