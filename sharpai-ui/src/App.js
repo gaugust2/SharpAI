1e4f5466ddc685147947c42c90e37ed4e659f36b
keremhatib <keremts01@hotmail.com>
2025-01-15 19:38:22 -0500

    added loading indicator to the chat page

--- sharpai-ui/src/App.js
+++ sharpai-ui/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import ChatPage from "./components/ChatPage";
